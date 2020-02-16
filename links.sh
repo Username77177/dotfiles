339fd4dc3199de8b66bc19ecbc3a4ce41a77cd70
username77177 <username77177@gmail.com>
2020-02-16 21:07:19 +0300

    16/02/2020 21:07:16 | STABLE

--- links.sh
+++ links.sh
@@ -4,7 +4,7 @@ rm i3config
 cat i3/* >> i3config
 rm -rf ~/.config/i3/ && mkdir ~/.config/i3/ && ln -sfr i3config ~/.config/i3/config
 ln -sfr XRESOURCES-for-all ~/.Xresources
-rm -rf ~/.config/dunst/ && rm .config/compton.conf && mkdir ~/.config/dunst && ln -sfr dunst ~/.config/dunst/dunstrc && ln -sfr compton.conf ~/.config/compton.conf
+rm -rf ~/.config/dunst/ && rm .config/compton.conf && mkdir ~/.config/dunst && ln -sfr dunst ~/.config/dunst/dunstrc && ln -sfr compton ~/.config/compton.conf
 rm -rf ~/.config/i3status && mkdir ~/.config/i3status && ln -sfr i3status ~/.config/i3status/config
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
