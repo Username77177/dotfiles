6983fc1dfdada88ffd2ff3e22718ec0db1c2c701
username77177 <username77177@gmail.com>
2020-02-29 21:46:38 +0300

    29/02/2020 21:46:36

--- bspwm/install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install bspwm sxhkd
-mkdir $HOME/.config/bspwm
-mkdir $HOME/.config/sxhkd
-ln -sfr bspwmrc ~/.config/bspwm/bspwmrc
-ln -sfr sxhkdrc ~/.config/sxhkd/sxhkdrc
-chmod +x $HOME/.config/bspwm/bspwmrc
-chmod +x $HOME/.config/sxhkd/sxhkdrc
